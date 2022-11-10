392ec5c475b9722ede5184abf5c303be62336362
Jorge Munera <gerjo9211@hotmail.com>
2022-11-10 08:50:20 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -18,21 +18,23 @@ async function fetchData(urlApi){
 
 (async ()=>{
     try{
-        const posts= await fetchData(API);
+        const videos= await fetchData(API);
         let view= `
-        ${posts.items.map(post=>`
+        ${videos.items.map(video=>`
+        <a href="https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
         <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${post.snippet.thumbnails.high.url}" alt="${post.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
           </div>
           <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
               <span aria-hidden="true" class="absolute inset-0"></span>
-              ${post.snippet.title}
+              ${video.snippet.title}
             </h3>
           </div>
         </div>
+        </a>
         `).slice(1,9).join(' ')}
         `;
         content.innerHTML = view;
